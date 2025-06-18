36220c44601fbe955c5715fdbb8e2adc6090d45a
santimb96 <santiagomartinezbota@gmail.com>
2025-06-18 13:30:52 +0200

    [feat]: added fetch priority to the profile image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   const START_YEAR = 2021
   return (
     <section id='aboutme' className='h-full scroll-m-28 mt-20 flex flex-col items-center gap-5 lg:flex-row lg:items-center lg:justify-center group'>
-      <img src={linkedinPic} alt='Profile picture' className='profileImg grayscale lg:group-hover:grayscale-0 duration-300 ease-in-out' />
+      <img src={linkedinPic} loading='eager' alt='Profile picture' className='profileImg grayscale lg:group-hover:grayscale-0 duration-300 ease-in-out' />
       <div className='flex flex-col justify-between h-full'>
         <h1>Santi Martínez</h1>
         <p className='presentation'>Programador web con <strong>+ de {new Date().getFullYear() - START_YEAR} años de experiencia</strong> como desarrollador <strong><i>front-end</i></strong>, así como en el <strong>diseño de interfaces y experiencia de usuario</strong>. Keep it simple 😉</p>
