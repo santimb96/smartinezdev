82d0845486006e19f1208c2034e44b8fb429f7db
santimb96 <santiagomartinezbota@gmail.com>
2025-06-18 14:29:11 +0200

    [fix]: removed whatsapp share link from contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,5 @@
-import { Copy, CopyCheck, Github, Mail } from 'lucide-preact'
+import { Copy, CopyCheck, Mail, Github } from 'lucide-preact'
 import LinkedInIcon from '../icons/LinkedInIcon.jsx'
-import WhatsAppIcon from '../icons/WhatsAppIcon.jsx'
 import { useState } from 'preact/hooks'
 
 const Contact = () => {
@@ -56,10 +55,7 @@ const Contact = () => {
                 <p className='text-center w-full md:w-3/4 lg:w-1/2'>
                     ¿Te ha gustado mi portfolio? Sería de gran ayuda que lo compartas con tu circulo de contactos. ¡Muchísimas gracias!
                 </p>
-                <div className='mt-10 flex justify-center items-center gap-5'>
-                    <a href='whatsapp://send?text=¡Hola! Me gustaría compartir contigo el portfolio de Santiago Martínez' target='_blank' rel='noreferrer' aria-label='Compartir por WhatsApp' className='min-w-6 hover:text-[var(--detail)] duration-150 ease-in-out'>
-                        <WhatsAppIcon width='46px' height='46px' />
-                    </a>
+                <div className='mt-5 flex justify-center items-center gap-5'>
                     <a href="https://www.linkedin.com/shareArticle?url=https://smartinezdev.com&title=Portfolio%20de%20Santiago%20Martínez&summary=Un%20portfolio%20desarrollado%20con%20ReactJS%20y%20TailwindCSS.&source=SmartinezDev" target="_blank" rel='noreferrer' aria-label='Compartir por LinkedIn' className='min-w-6 hover:text-[var(--detail)] duration-150 ease-in-out'>
                         <LinkedInIcon width='36px' height='36px' />
                     </a>
